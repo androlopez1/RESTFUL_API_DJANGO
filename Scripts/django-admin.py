00886359a00f38aecdecf2e403916bbb357e3022
androlopez1 <androlopez1@gmail.com>
2017-07-05 20:04:52 -0500

    'DJANGO'

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\archivador\desktop\andrés\test\napice\napice_virtualenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
